708e9711e86d061492b6413751d4008cc396adc7
Ashish Kumar Bostan <ashishkumar.bostan@siemens-healthineers.com>
2025-04-07 10:53:10 +0530

    utility updates

--- CCRManager/Utils/UtilityFunctions.cs
+++ CCRManager/Utils/UtilityFunctions.cs
@@ -5,19 +5,11 @@ namespace CCRManager.Utils
 {
     public static class UtilityFunctions
     {
-        //public static string ConvertDateTimeToIso8601(DateTime input)
-        //{
-        //    // Ensure the DateTime is in UTC before formatting
-        //    return input.ToUniversalTime().ToString("o");
-        //}
         public static string ConvertToIso8601(string time)
         {
             // Define the expected input format, e.g., "MM/dd/yyyy HH:mm:ss"
             string format = "MM/dd/yyyy HH:mm:ss";
-            DateTime dateTime;
-
-            // Try to parse the user input according to the specified format.
-            if (DateTime.TryParseExact(time, format, CultureInfo.InvariantCulture, DateTimeStyles.AssumeLocal, out dateTime))
+            if (DateTime.TryParseExact(time, format, CultureInfo.InvariantCulture, DateTimeStyles.AssumeLocal, out DateTime dateTime))
             {
                 return dateTime.ToUniversalTime().ToString("o");
             }
@@ -28,10 +20,8 @@ namespace CCRManager.Utils
         }
         public static string PrettyPrintJson(string json)
         {
-            using (JsonDocument doc = JsonDocument.Parse(json))
-            {
-                return JsonSerializer.Serialize(doc, new JsonSerializerOptions { WriteIndented = true });
-            }
+            using JsonDocument doc = JsonDocument.Parse(json);
+            return JsonSerializer.Serialize(doc, new JsonSerializerOptions { WriteIndented = true });
         }
     }
 }
